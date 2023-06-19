82cf62b90b145da4054e6790d683d61e115569bf
Mateus Arbex <51658233+mateusarbex@users.noreply.github.com>
2023-06-18 22:47:54 -0400

    Add font family

---
+++ src/assets/static-plugin/dist/add-styles.js
@@ -0,0 +1,52 @@
+var observeDOM = (function () {
+  var MutationObserver =
+    window.MutationObserver || window.WebKitMutationObserver;
+
+  return function (obj, callback) {
+    if (!obj || obj.nodeType !== 1) return;
+
+    if (MutationObserver) {
+      // define a new observer
+      var mutationObserver = new MutationObserver(callback);
+
+      // have the observer observe for changes in children
+      mutationObserver.observe(obj, { childList: true, subtree: true });
+      return mutationObserver;
+    }
+
+    // browser support fallback
+    else if (window.addEventListener) {
+      obj.addEventListener("DOMNodeInserted", callback, false);
+      obj.addEventListener("DOMNodeRemoved", callback, false);
+    }
+  };
+})();
+
+let shouldChange = 0;
+
+observeDOM(document.body, () => {
+  const content = document.querySelector(".content");
+  const visible = document.querySelector(".css-68ai3g.visible");
+  const fontWeight = document.querySelector("#fontWeight");
+
+  if (content && visible && !fontWeight) {
+    const textToQuery = content.textContent;
+    for (let tspan of document.querySelectorAll("text tspan")) {
+      if (tspan.textContent === textToQuery) {
+        const css = document.querySelector(
+          ".css-68ai3g.visible .content > div"
+        );
+        const spanAttribute = document.createElement("span");
+        spanAttribute.id = "fontWeight";
+        spanAttribute.className = "hljs-attribute";
+        spanAttribute.textContent = "font-family: ";
+        const spanValue = document.createElement("span");
+        spanValue.className = "hljs-number";
+        spanValue.textContent = getComputedStyle(tspan).fontFamily;
+
+        css.appendChild(spanAttribute);
+        css.appendChild(spanValue);
+      }
+    }
+  }
+});
